5557bbc3dba6d6728451b9d57dcd036719f37322
Easin Arafat <arafatmrx@gmail.com>
2021-08-02 15:05:45 +0600

    parseInt(string)

---
+++ Basic/basic.js
@@ -0,0 +1,11 @@
+var num= 6969;
+var numString='100';
+
+console.log(typeof num); // type number 
+//NB: typeof not the camel-case
+console.log(typeof numString) //type string 
+
+
+//parseInt() - To convert String into a Number 
+var numParsed=parseInt (numString) //type number
+console.log(typeof numParsed) 
