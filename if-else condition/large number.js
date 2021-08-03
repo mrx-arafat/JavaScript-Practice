d4d523de23b2f44a8008e6e18bdfb8f943312636
Easin Arafat <arafatmrx@gmail.com>
2021-08-03 07:43:06 +0600

    large number

---
+++ if-else condition/large number.js
@@ -0,0 +1,11 @@
+var num1 = 212;
+var num2 = 397;
+var num3 = 111;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("Num-1 is largest");
+} else if (num2 > num1 && num2 > num3) {
+  console.log("Num-2 is largest");
+} else {
+  console.log("Num-3 is largest");
+}
