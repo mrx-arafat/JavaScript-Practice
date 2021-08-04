666395234bf66244897a6064e67c0c4cf1a3b4d7
Easin Arafat <arafatmrx@gmail.com>
2021-08-04 13:21:18 +0600

    push pop

---
+++ 4. Array/arrayPushPop.js
@@ -0,0 +1,15 @@
+let color = ["red", "green", "blue", "orange"];
+console.log(color);
+console.log(color.length);
+
+//push
+color.push("purple");
+
+console.log(color);
+console.log(color.length);
+// run in chrome debug console
+
+//pop()
+color.pop();
+console.log(color);
+console.log(color.length);
---
+++ 4. Array/arraybasic.js
@@ -0,0 +1,9 @@
+let colorS = ["red", "green", "blue", "orange"];
+console.log(colorS.length);
+
+//find value of array index
+
+console.log(colorS[2]);
+
+var last = colorS[colorS.length - 1];
+console.log(last);
