f345bb47825d01821e33f60113e76284f5fed450
Easin Arafat <arafatmrx@gmail.com>
2021-08-05 17:24:56 +0600

    permutation

---
+++ 3. Loop Control Statement/nestedLoop.js
@@ -0,0 +1,7 @@
+// multipication table 1-20
+
+for (var n = 1; n <= 20; n++) {
+  for (var i = 1; i <= 10; i++) {
+    console.log(n, "x", i, "=", n * i);
+  }
+}
---
+++ 3. Loop Control Statement/permutation 1 2 3.js
@@ -0,0 +1,10 @@
+for (var i = 1; i <= 3; i++) {
+  for (var j = 1; j <= 3; j++) {
+    for (var k = 1; k <= 3; k++) {
+      console.log(i, j, k);
+    }
+  }
+}
+// 1 2 3 total 3 number that's why 3 nested loop
+
+// for 4 we will run 4 nested loop
