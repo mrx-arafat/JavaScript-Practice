e3dbb612bd0cd0c2270ff10ee9e4d06b340e70e1
Easin Arafat <arafatmrx@gmail.com>
2021-08-06 12:03:40 +0600

    push

---
+++ JS Problem Solving/a. kmToMeter.js
@@ -0,0 +1,9 @@
+function kmToMeter(km) {
+  var meter = 1000 * km;
+
+  return meter;
+}
+
+console.log(kmToMeter(4));
+
+//
---
+++ JS Problem Solving/a. min max.js
@@ -0,0 +1,9 @@
+const array = [2, 3, 1];
+
+console.log(Math.max(...array));
+//
+const a = 2,
+  b = 3,
+  c = 1;
+
+console.log(Math.min(a, b, c));
