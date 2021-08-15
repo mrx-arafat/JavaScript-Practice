62edcbd4845afcc1c4934f887660e5e98f82d561
Easin Arafat <arafatmrx@gmail.com>
2021-08-15 23:30:35 +0600

    obj2

---
+++ object2.js
@@ -0,0 +1,15 @@
+// run in chrome
+
+const zoo = {
+  lion: "🦁",
+  panda: "🐼",
+};
+
+Object.keys(zoo);
+// ['lion', 'panda']
+
+Object.values(zoo);
+// ['🦁', '🐼']
+
+Object.entries(zoo);
+// [ ['lion', '🦁'], ['panda', '🐼'] ]
